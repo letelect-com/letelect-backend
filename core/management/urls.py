273c860a2770210e7f000085eb98e3b2886bdfc5
DevPrinceK <princesamuelpks@gmail.com>
2023-05-13 12:24:54 +0000

    Update urls.py

--- core/management/urls.py
+++ core/management/urls.py
@@ -1,5 +1,12 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/", include("management.urls")),
+    path("admin/", admin.site.urls),
+    path("api-v1/", include("management.urls")),
+]
+
+# default auth url for rest_framework
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
