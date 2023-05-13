9528985b4d257ee30b479105ba8a26f030f0191b
DevPrinceK <princesamuelpks@gmail.com>
2023-05-13 13:53:13 +0000

    Update urls.py

---
+++ core/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from knox import views as knox_views
+from . import views
+
+app_name = 'api'
+
+# overview of all urls
+urlpatterns = [
+    path('', views.TestAPI.as_view(), name='test'),
+    path('test/', views.TestAPI.as_view(), name='test'),
+]
+
+# accounts urls
+urlpatterns += [
+    path('login/', views.LoginAPI.as_view(), name='login'),
+    path('signup-client/', views.SignUpClientAPI.as_view(), name='signup-client'),
+    path('register-staff/', views.RegisterStaffAPI.as_view(), name='register-staff'),
+    path('change-password/', views.ChangePasswordAPI.as_view(), name='change-password'),  # noqa
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+]
